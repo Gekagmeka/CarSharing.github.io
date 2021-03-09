dbba1e69e6e0f6a7430e0ba438d4d456ca72633a
Gekagmeka <shulgagggeka@gmail.com>
2021-03-09 13:54:36 +0200

    added body locked for mobile

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,9 @@
 //navigation (add/remove class)
 var navigation = document.querySelector('.nav');
 var burger_menu = document.querySelector('.burger-menu');
+var body_main = document.querySelector('body');
 
 burger_menu.onclick = function() {
    navigation.classList.toggle('active');
+   body_main.classList.toggle('locked');
 }
